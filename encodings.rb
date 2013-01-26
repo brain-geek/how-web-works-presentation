e7ee290fe624a3fa698604a839c4b1b616eaebed
Alex Rozumey <brain-geek@yandex.ua>
2013-01-26 10:04:38 +0200

    Add slides about encodings

--- encodings.rb
+++ encodings.rb
@@ -1,15 +1,14 @@
 #encoding: utf-8
 
-['a', 'а', "ё", "е"].each do |char|
-  puts "========Analyzing letter '#{char}'========"
+ARGV.each do |str|
 
-  def analyze(char, encoding)
+  def analyze(str, encoding)
     puts "Encoding: #{encoding}"
 
-    puts char.encode(encoding).codepoints.first.inspect
+    puts str.encode(encoding).force_encoding('ASCII-8BIT').codepoints.to_a.inspect
   end
 
   ["UTF-8", "Windows-1251", "KOI8-R"].each do |a|
-    analyze char, a
+    analyze str.dup.force_encoding('UTF-8'), a
   end
 end
